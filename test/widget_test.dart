8e034708fa2c41facbd0b2fe2eec43618ad6d51e
Diego Bernardo <diegobsiufrpe@gmail.com>
2024-12-01 20:28:41 -0300

    Update main e widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:filmin/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const FilmIn());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
