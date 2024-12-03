b44203b3c56aef68362da71f55ede5db60bde7b1
lpauloaraujo <158222698+lpauloaraujo@users.noreply.github.com>
2024-12-03 14:44:45 -0300

    Finalizando o registro e configuração do app ao firebase
    importações do firebase e código para a inicialização do firebase adicionados ao arquivo main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:filmin/login.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized(); 
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+
   runApp(const FilmIn());
 }
 
