f745f893eeca5a0eba7ec93f9b8e557e00463f15
lpauloaraujo <158222698+lpauloaraujo@users.noreply.github.com>
2024-12-05 15:43:46 -0300

    avanços na tela de criticas

--- lib/criticas.dart
+++ lib/criticas.dart
@@ -1,27 +1,40 @@
 import 'package:flutter/material.dart';
+import 'filme.dart';
 
 class CriticasScreen extends StatelessWidget {
+
   const CriticasScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(title: Text("Críticas")),
-      body: Column(
-        children: [],
+  List<Widget> get _criticas {
+    return List.generate(
+      5,
+      (index) => _buildCritica(
+        'Filme $index',
+        'Esta é a crítica para o filme $index.',
       ),
     );
   }
 
-  Widget _buildCritica() {
+  @override
+  Widget build(BuildContext context) {
+    
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text("Críticas")),
+      body: Column(
+        children: _criticas,
+      ),
+    );
+  }
+
+  Widget _buildCritica(String titulo_filme, String texto_critica) {
     return Column(
-        children: [
-            Text("Título do Filme"),
-            Row(
-                children: [filme, Text("Texto da crítica")],
-            )
-        ],
+      children: [
+        Text("Título do Filme"),
+        Row(
+          children: [FilmeWidget(titulo: titulo_filme), Text(texto_critica)],
+        )
+      ],
     );
   }
 }
