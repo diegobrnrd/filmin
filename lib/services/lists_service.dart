086030cb7ed4b46a2cf8f3e67631b411910d716e
lpauloaraujo <158222698+lpauloaraujo@users.noreply.github.com>
2025-02-08 17:52:39 -0300

    adicionando lógica para checar se o filme ja ta na lista antes de adicioná-lo

--- lib/services/lists_service.dart
+++ lib/services/lists_service.dart
@@ -33,122 +33,125 @@ class ListsService {
       if (listaQuery.docs.isNotEmpty) {
         final listaRef = listaQuery.docs.first.reference.collection('movies');
 
-        await listaRef.add({
-          'id': movie['id'],
-          'release_date': movie['release_date'],
-          'poster_path': movie['poster_path'],
-          'genres': movie['genres']
-              .map((genre) => {
-                    'id': genre['id'],
-                    'name': genre['name'],
-                  })
-              .toList(),
-          'runtime': movie['runtime'],
-          'dateAdded': Timestamp.now(),
-        });
-      }
-    }
-    }
+        final existingMovieQuery =
+            await listaRef.where('id', isEqualTo: movie['id']).get();
 
-    Future<void> deleteList(String documentId) async {
-      final User? user = _auth.currentUser;
-      if (user != null) {
-        await _firestore
-            .collection('users')
-            .doc(user.uid)
-            .collection('lists')
-            .doc(documentId)
-            .delete();
-      }
-    }
-
-    Future<void> deleteFromList(String movieId, String nomeLista) async {
-      final User? user = _auth.currentUser;
-      if (user != null) {
-        final listaQuery = await _firestore
-            .collection('users')
-            .doc(user.uid)
-            .collection('lists')
-            .where('name', isEqualTo: nomeLista)
-            .get();
-
-        if (listaQuery.docs.isNotEmpty) {
-          final listaRef = listaQuery.docs.first.reference.collection('movies');
-
-          listaRef.doc(movieId).delete();
-        }
-      }
-    }
-
-    Future<List<Map<String, dynamic>>> getList(String nomeLista) async {
-      final User? user = _auth.currentUser;
-      if (user != null) {
-        final listaQuery = await _firestore
-            .collection('users')
-            .doc(user.uid)
-            .collection('lists')
-            .where('name', isEqualTo: nomeLista)
-            .get();
-
-        final listaRef =
-            await listaQuery.docs.first.reference.collection('movies').get();
-
-        return listaRef.docs
-            .map((doc) => {
-                  'documentId': doc.id,
-                  'id': doc['id'],
-                  'release_date': doc['release_date'],
-                  'poster_path': doc['poster_path'],
-                  'genres': doc['genres'],
-                  'runtime': doc['runtime'],
-                  'dateAdded': doc['dateAdded'],
-                })
-            .toList();
-      } else {
-        return [];
-      }
-    }
-
-    Future<void> updateList(
-        String nomeAtual, String novoNome, String novaDescricao) async {
-      final User? user = _auth.currentUser;
-      if (user != null) {
-        final listaQuery = await _firestore
-            .collection('users')
-            .doc(user.uid)
-            .collection('lists')
-            .where('name', isEqualTo: nomeAtual)
-            .get();
-
-        final listaRef = listaQuery.docs.first.reference;
-
-        listaRef.update({
-          'name': novoNome,
-          'descricao': novaDescricao,
-        });
-      }
-    }
-
-    Future<List<Map<String, dynamic>>> getUserLists() async {
-      final User? user = _auth.currentUser;
-      if (user != null) {
-        final snapshot = await _firestore
-            .collection('users')
-            .doc(user.uid)
-            .collection('lists')
-            .get();
-
-
-        return snapshot.docs.map((doc) {
-          return {
-            'documentId': doc.id, // ID único da lista
-            'name': doc['name'], // Nome da lista
-            'description': doc['description'], // Descrição da lista
-          };
-        }).toList();
-      } else {
-        return []; // Retorna uma lista vazia se o usuário não estiver autenticado
+        if (existingMovieQuery.docs.isEmpty) {
+          await listaRef.add({
+            'id': movie['id'],
+            'release_date': movie['release_date'],
+            'poster_path': movie['poster_path'],
+            'genres': movie['genres']
+                .map((genre) => {
+                      'id': genre['id'],
+                      'name': genre['name'],
+                    })
+                .toList(),
+            'runtime': movie['runtime'],
+            'dateAdded': Timestamp.now(),
+          });
+        } 
       }
     }
   }
 
+  Future<void> deleteList(String documentId) async {
+    final User? user = _auth.currentUser;
+    if (user != null) {
+      await _firestore
+          .collection('users')
+          .doc(user.uid)
+          .collection('lists')
+          .doc(documentId)
+          .delete();
+    }
+  }
+
+  Future<void> deleteFromList(String movieId, String nomeLista) async {
+    final User? user = _auth.currentUser;
+    if (user != null) {
+      final listaQuery = await _firestore
+          .collection('users')
+          .doc(user.uid)
+          .collection('lists')
+          .where('name', isEqualTo: nomeLista)
+          .get();
+
+      if (listaQuery.docs.isNotEmpty) {
+        final listaRef = listaQuery.docs.first.reference.collection('movies');
+
+        listaRef.doc(movieId).delete();
+      }
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getList(String nomeLista) async {
+    final User? user = _auth.currentUser;
+    if (user != null) {
+      final listaQuery = await _firestore
+          .collection('users')
+          .doc(user.uid)
+          .collection('lists')
+          .where('name', isEqualTo: nomeLista)
+          .get();
+
+      final listaRef =
+          await listaQuery.docs.first.reference.collection('movies').get();
+
+      return listaRef.docs
+          .map((doc) => {
+                'documentId': doc.id,
+                'id': doc['id'],
+                'release_date': doc['release_date'],
+                'poster_path': doc['poster_path'],
+                'genres': doc['genres'],
+                'runtime': doc['runtime'],
+                'dateAdded': doc['dateAdded'],
+              })
+          .toList();
+    } else {
+      return [];
+    }
+  }
+
+  Future<void> updateList(
+      String nomeAtual, String novoNome, String novaDescricao) async {
+    final User? user = _auth.currentUser;
+    if (user != null) {
+      final listaQuery = await _firestore
+          .collection('users')
+          .doc(user.uid)
+          .collection('lists')
+          .where('name', isEqualTo: nomeAtual)
+          .get();
+
+      final listaRef = listaQuery.docs.first.reference;
+
+      listaRef.update({
+        'name': novoNome,
+        'descricao': novaDescricao,
+      });
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getUserLists() async {
+    final User? user = _auth.currentUser;
+    if (user != null) {
+      final snapshot = await _firestore
+          .collection('users')
+          .doc(user.uid)
+          .collection('lists')
+          .get();
+
+      return snapshot.docs.map((doc) {
+        return {
+          'documentId': doc.id, // ID único da lista
+          'name': doc['name'], // Nome da lista
+          'description': doc['description'], // Descrição da lista
+        };
+      }).toList();
+    } else {
+      return []; // Retorna uma lista vazia se o usuário não estiver autenticado
+    }
+  }
+}
