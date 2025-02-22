2b6eae228faca4e20ecf5bf8ee923a9ba078bab4
Diego Bernardo <diegobsiufrpe@gmail.com>
2025-02-22 02:56:18 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:filmin/screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:filmin/firebase_options.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:filmin/secrets.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,8 +11,8 @@ Future<void> main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await Supabase.initialize(
-    url: 'https://hleacgrlmrkhmvuaechi.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsZWFjZ3JsbXJraG12dWFlY2hpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAxNzgyNjIsImV4cCI6MjA1NTc1NDI2Mn0.a2hcwGotCfULOVsmdVf5o3m82nVsTAug0hpFqrgFzh0',
+    url: url,
+    anonKey: anonKey,
   );
   runApp(const FilmIn());
 }
