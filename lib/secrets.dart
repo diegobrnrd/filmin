ffc7fd103db08a11d23cad97b3d0c3b651f1383f
Diego Bernardo <diegobsiufrpe@gmail.com>
2025-03-09 19:04:19 -0300

    Commit temporário

---
+++ lib/secrets.dart
@@ -0,0 +1,3 @@
+const String apiKey = '06a0a8c5d4aea74cffbb9e3275112238';
+const String url = 'https://lfxaolvwfttemhtzrgih.supabase.co';
+const String anonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxmeGFvbHZ3ZnR0ZW1odHpyZ2loIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAyMTgyMzksImV4cCI6MjA1NTc5NDIzOX0.wDtZGuLkfK7p_sv--VNtyMWK8dIs2xb6R6bY8BfzGRQ';
